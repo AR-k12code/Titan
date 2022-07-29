c79a645d38e11bea195ef18c8538f66850224889
Cweb <charlesweber85@gmail.com>
2022-07-29 09:13:31 -0500

    Update settings_sample.ps1

--- resources/settings_sample.ps1
+++ resources/settings_sample.ps1
@@ -1,2 +1,6 @@
 $username = "Gentry_SIS"
 $password = 'ZQy#vRr#r(Z}9x\u'
+
+#$LockerCode pulls down Titan_Locker_serving_number from Cognos. 
+#This is the same file as Titan; except the Locker Number is the last Column.
+#Some customers use the Locker Number field to be the serving number used in Titan.
